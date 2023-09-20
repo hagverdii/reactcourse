6274aff6148205e0516da177336930d73bb7d935
Mamed <memmed.haqverdi02@gmail.com>
2023-09-20 09:52:51 +0400

    new files

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 
 const Header = () => {
-    const handleNameChange = () => {
-        const names = ['Bob', 'Kevin', 'Mamed', 'Dave'];
-        const int = Math.floor(Math.random() * 4);
-        return names[int];
-    }
-    
     return (
         <header>
             <h1>Groceries List</h1>
